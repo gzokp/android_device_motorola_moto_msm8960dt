e79577cd10f7bbfc8fb6f6364935817af3de538f
Validus13 <groundzeroroms@gmail.com>
2014-03-19 18:38:53 -0400

    Update and rename gzr.mk to carbon.mk

--- carbon.mk
+++ carbon.mk
@@ -1,14 +1,14 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/gzr/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/gzr/config/nfc_enhanced.mk)
+# $(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
-PRODUCT_NAME := gzr_moto_msm8960dt
+PRODUCT_NAME := carbon_moto_msm8960dt
 
 $(call inherit-product, device/motorola/moto_msm8960dt/full_moto_msm8960dt.mk)
