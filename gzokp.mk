ebc88a0307df44b0918298d796407d7ca9d3d51c
Validus13 <groundzeroroms@gmail.com>
2014-03-22 17:56:00 -0400

    Update and rename carbon.mk to gzokp.mk

--- gzokp.mk
+++ gzokp.mk
@@ -1,5 +1,5 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+$(call inherit-product, vendor/gzokp/config/common_phone.mk)
 
 # Enhanced NFC
 # $(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
@@ -9,6 +9,6 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
-PRODUCT_NAME := carbon_moto_msm8960dt
+PRODUCT_NAME := gzokp_moto_msm8960dt
 
 $(call inherit-product, device/motorola/moto_msm8960dt/full_moto_msm8960dt.mk)
